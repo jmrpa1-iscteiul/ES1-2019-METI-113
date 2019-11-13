8cb1d8148cd8c5a2f9896e44254ebd3fb33e5dd3
Tiago Costa <jtbpc@iscte-iul.pt>
2019-11-13 19:42:54 +0000

    teste sysout

--- ES1_2019_METI_113/src/projeto/ClasseTeste.java
+++ ES1_2019_METI_113/src/projeto/ClasseTeste.java
@@ -6,5 +6,7 @@ public class ClasseTeste {
 	public static void main(String[] args) {
 		System.out.println("Olá - João Praia");
 		//Olá 2
+		//Teste Tiago
+		System.out.println("Teste - Tiago Costa");
 	}
 }
